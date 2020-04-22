de430e723f157b94d261f71dd3883ce275c2eeee
lukasziegler <lukas@lukasziegler.com>
2020-04-22 22:38:30 +0200

    Changing `gatsby build` to deploy to `docs/`
    Allowing us to run Github pages from master branch, while keeping the source code in the same file.
    
    Related resources:
    https://github.com/gatsbyjs/gatsby/issues/18975
    https://help.github.com/articles/configuring-a-publishing-source-for-github-pages/

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+ const path = require("path")
+ const fs = require("fs")
+
+ exports.onPreInit = () => {
+   if (process.argv[2] === "build") {
+     fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+     fs.renameSync(
+       path.join(__dirname, "public"),
+       path.join(__dirname, "public_dev")
+     )
+   }
+ }
+
+ exports.onPostBuild = () => {
+   fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
+   fs.renameSync(
+     path.join(__dirname, "public_dev"),
+     path.join(__dirname, "public")
+   )
+ }
