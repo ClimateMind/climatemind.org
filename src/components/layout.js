33e48a7aac079de3483ba2fba981bc7c63ee6bcd
lukasziegler <lukas@lukasziegler.com>
2020-04-22 22:23:26 +0200

    First adjustments to Gatsby starter kit

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,12 +34,18 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
+      <footer style={{
+        backgroundColor: `#2B9F99`,
+        padding: `1.45rem 1.0875rem`,
+      }}>
+        <div style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+        }}>
+          © {new Date().getFullYear()}
+        </div>
+      </footer>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome</h1>
+    <p>We are still at the very beginning. Come back soon to find out more about our endeavours.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
