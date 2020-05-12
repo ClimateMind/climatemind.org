44d8185d7544a61ce15a968c9174abf37bf7ae2b
lukasziegler <lukas@lukasziegler.com>
2020-05-12 19:42:24 +0200

    Google Analytics
    and updates to our links in the resources section of
    the footer & for #climatemind in the hero.

--- docs/app.js
+++ docs/app.js
@@ -10,6 +10,12 @@ var app = (function () {
 
 })();
 
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-165706180-1');
+
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
