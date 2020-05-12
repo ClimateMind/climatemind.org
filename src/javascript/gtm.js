916df3ff73463096016f4fa13f1541c5364b0d9b
lukasziegler <lukas@lukasziegler.com>
2020-05-12 08:21:31 +0200

    Google Tag Manager
    Adding init scripts for GTM
    and minor tweak to hero, making the SVG fill the container.

--- src/javascript/gtm.js
+++ src/javascript/gtm.js
@@ -1,15 +1,3 @@
-var app = (function () {
-
-  var init = function() {
-    console.log('hello world');
-  };
-
-  return {
-    init: init
-  };
-
-})();
-
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
